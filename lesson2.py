a4fcc7d8c10e62520c552fa66584f3583ee9099b
katairud <rudenko.catherina@gmail.com>
2022-09-04 23:51:47 +0300

    data type1

--- lesson2.py
+++ lesson2.py
@@ -5,9 +5,9 @@ per4 = 74.12
 per5 = "Paty"
 per6 = "open"
 per7 = True
-pen8 = True
-pen9 = False
-pen10 = False
-pen11 = None
-pen12 = None
+per8 = True
+per9 = False
+per10 = False
+per11 = None
+per12 = None
 print(per1,per2,per3,per4,per5,per6,per7,per8,per9,per10,per11,per12)
