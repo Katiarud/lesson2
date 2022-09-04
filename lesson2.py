3f07057f0c8d402c43cde2d5118af52dc263ea11
katairud <rudenko.catherina@gmail.com>
2022-09-04 15:32:54 +0300

    data type

--- lesson2.py
+++ lesson2.py
@@ -1,1 +1,19 @@
 kvjhgdjg
+a=17 # int
+print(a)
+b=320 # int
+print(b)
+c=32.2 3 # floot
+print(c)
+d=574.12 # flootoat
+print(d)
+e="Paty" # str
+print(e)
+f="open" # srt
+print(f)
+g=True # bool
+print(g)
+h=False # bool
+print(h)
+j=None # пустое значение
+print(j)
